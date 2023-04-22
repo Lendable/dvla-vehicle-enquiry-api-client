142509278ef85cb8397475ec0f396c348b5f70ce
Ben Challis <ben-challis@users.noreply.github.com>
2023-04-22 11:06:58 +0100

    Convert TaxStatus to native enum (#178)

--- src/Scope/VehiclesScope/ValueObject/TaxStatus.php
+++ src/Scope/VehiclesScope/ValueObject/TaxStatus.php
@@ -4,49 +4,10 @@ declare(strict_types=1);
 
 namespace Lendable\Dvla\VehicleEnquiry\Scope\VehiclesScope\ValueObject;
 
-use Assert\Assert;
-
-final class TaxStatus
+enum TaxStatus: string
 {
-    public const NOT_TAXED_FOR_ON_ROAD_USE = 'Not Taxed for on Road Use';
-    public const SORN = 'SORN';
-    public const TAXED = 'Taxed';
-    public const UNTAXED = 'Untaxed';
-
-    private const ALL = [
-        self::NOT_TAXED_FOR_ON_ROAD_USE => 1,
-        self::SORN => 1,
-        self::TAXED => 1,
-        self::UNTAXED => 1,
-    ];
-
-    /**
-     * @var array<string, self>
-     */
-    private static array $lazyLoad = [];
-
-    private function __construct(private readonly string $value)
-    {
-        Assert::that(self::ALL)->keyIsset($this->value);
-    }
-
-    public static function fromString(string $value): self
-    {
-        return self::lazyLoad($value);
-    }
-
-    private static function lazyLoad(string $value): self
-    {
-        return self::$lazyLoad[$value] ??= new self($value);
-    }
-
-    public function equals(self $other): bool
-    {
-        return $this->value === $other->value;
-    }
-
-    public function toString(): string
-    {
-        return $this->value;
-    }
+    case NOT_TAXED_FOR_ON_ROAD_USE = 'Not Taxed for on Road Use';
+    case SORN = 'SORN';
+    case TAXED = 'Taxed';
+    case UNTAXED = 'Untaxed';
 }
